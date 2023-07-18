ffcdddc586c8d7b829fdd05f95c8dd883c1a1c78
Halibei Marian <marianhalibei123@gmail.com>
2023-07-18 14:56:28 +0200

    Progress on currencies: XXX test passing

--- currencies.py
+++ currencies.py
@@ -1,11 +1,23 @@
 # pylint: disable=missing-docstring
 
 # TODO: add some currency rates
-RATES = {}
+RATES = {
+
+    "USDEUR": 0.85,
+    "GBPEUR": 1.13,
+    "CHFEUR": 0.86,
+    "EURGBP": 0.885
+}
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
     amount is a tuple like (100, "EUR")
     currency is a string
     """
-    pass  # YOUR CODE HERE
+    value = amount[0]
+    origin_currency = amount[1]
+    target_currency = currency
+    total_currency = origin_currency + target_currency
+
+    if total_currency in RATES:
+       return round(value * RATES[total_currency])
